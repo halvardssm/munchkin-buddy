a843c6cdfa46fde0b05b455e46668953ae655db6
halvardssm <halvard@simplyundoable.com>
2019-07-26 22:09:12 +0200

    Included old app

--- app/src/main/java/com/simplyundoable/munchkinbuddy/MainActivity.kt
+++ app/src/main/java/com/simplyundoable/munchkinbuddy/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.simplyundoable.munchkinbuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View) {
+        // Get the text view
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString()
     }
 }
